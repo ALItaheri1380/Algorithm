af794cd178b833583894241884e90164a23bb49b
Ali Taheri <98982133+ALItaheri1380@users.noreply.github.com>
2023-06-27 14:09:31 +0330

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -12,66 +12,66 @@ const int maxn=2*1e5+50;
 int row, col;
 
 bool chekdefl(int stage [N][N],int& row, int& col){
-	for (row = 0; row < N; row++)
-		for (col = 0; col < N; col++)
-			if (stage [row][col] == defl)
-				return true;
-	return false;
+     for (row = 0; row < N; row++)
+	for (col = 0; col < N; col++)
+	     if (stage [row][col] == defl)
+		return true;
+     return false;
 }
 bool CheckRow(int stage [N][N], int row, int i){
-	for (int col = 0; col < N; col++)
-		if (stage [row][col] == i)
-			return true;
-	return false;
+     for (int col = 0; col < N; col++)
+        if (stage [row][col] == i)
+		return true;
+     return false;
 }
 
 bool CheckCol(int stage [N][N], int col, int i){
-	for (int row = 0; row < N; row++)
-		if (stage [row][col] == i)
-			return true;
-	return false;
+     for (int row = 0; row < N; row++)
+	if (stage [row][col] == i)
+	    return true;
+     return false;
 }
 
 bool CheckEStage(int stage [N][N], int boxStartRow,int boxStartCol, int i){
-	for (int row = 0; row < 3; row++)
-		for (int col = 0; col < 3; col++)
-			if (stage [row + boxStartRow][col + boxStartCol] ==i) return true;
-	return false;
+     for (int row = 0; row < 3; row++)
+	for (int col = 0; col < 3; col++)
+	    if (stage [row + boxStartRow][col + boxStartCol] ==i) return true;
+     return false;
 }
 
 void print(int stage [N][N]){
-	for (int row = 0; row < N; row++)
-		for (int col = 0; col < N; col++)
-			cout << stage [row][col] << " ";
-		cout << endl;
+     for (int row = 0; row < N; row++)
+         for (int col = 0; col < N; col++)
+		cout << stage [row][col] << " ";
+     cout << endl;
 }
 
 bool isSafe(int stage [N][N], int row,int col, int i){
-	return !CheckRow(stage , row, i)
-		&& !CheckCol(stage , col, i)
-		&& !CheckEStage(stage , row - row % 3,col - col % 3, i)
-		&& stage [row][col] == defl;
+     return !CheckRow(stage , row, i)
+	&& !CheckCol(stage , col, i)
+	&& !CheckEStage(stage , row - row % 3,col - col % 3, i)
+	&& stage [row][col] == defl;
 }
 
 bool Sudoku(int stage [N][N]){
-	if (!chekdefl(stage , row, col)) return true;
+     if (!chekdefl(stage , row, col)) return true;
 	for (int i = 1; i <= 9; i++){
-		if (isSafe(stage , row, col, i)){
-			stage [row][col] = i;
-			if (Sudoku(stage )) return true;
-		        stage [row][col] = defl;
-		}
+	    if (isSafe(stage , row, col, i)){
+		stage [row][col] = i;
+		if (Sudoku(stage )) return true;
+	        stage [row][col] = defl;
+	    }
 	}
-	return false;
+      return false;
 }
 
 /***************************************************/
 int main()
 {
-	int stage [N][N];cout<<"Input your :\n";
-	for(int i = 0 ; i < N ; i++)
-		for(int j = 0 ; j < N ; j++)
-			cin>>stage[i][j];			
+    int stage [N][N];cout<<"Input your :\n";
+    for(int i = 0 ; i < N ; i++)
+	for(int j = 0 ; j < N ; j++)
+		cin>>stage[i][j];			
 	if(Sudoku(stage)) print(stage);
 	else cout<<"Invalid input!!!";
 
